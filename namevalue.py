50f755a31de8541db588aad1b317e354c97f4148
Abhishek Varanasi <varanasi.abhishek@outlook.com>
2017-08-11 12:27:32 -0500

    added one more method

--- namevalue.py
+++ namevalue.py
@@ -1,10 +1,11 @@
-atoz = ([chr(i) for i in range(ord("a"), ord("z")+1)])
+import string
+
+atoz = ([chr(i) for i in range(ord("a"), ord("z")+1)])  #Method 1
+ch = list(string.ascii_lower) #Method 2
 name = input()
 value = 0
 for i in name:
   for v,ch in enumerate(atoz):
-    if i == ch:
+    if i.lower() == ch:
       value += v+1
-
 print(value)
-    
